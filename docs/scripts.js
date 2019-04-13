8c8c949d97d9dff634cf81bf780191fa5cdaa532
Axel Kesseler <axel.kesseler@gmx.net>
2019-04-14 01:17:59 +0200

    Initial draft.

---
+++ docs/scripts.js
@@ -0,0 +1,93 @@
+﻿
+function setCopyright(selector) {
+
+    if (selector) {
+
+        selector = '#' + selector;
+
+        let date = new Date();
+        let year = date.getFullYear();
+        let text = 'Copyright&nbsp;&copy;&nbsp;' + year + '&nbsp;<a href="http://www.plexdata.de" target="_blank">plexdata.de</a>';
+
+        $(selector).html(text);
+    }
+}
+
+function makeSelector(value) {
+
+    const hash = '#';
+
+    if (value) {
+
+        let index = value.indexOf(hash);
+
+        if (index < 0) {
+            return hash + value;
+        }
+    }
+
+    return value;
+}
+
+function selectCaller(caller) {
+
+    if (caller) {
+
+        try {
+
+            let parent = $(caller).parent();
+
+            if (parent) {
+
+                $(parent).children('a').each(function () {
+                    $(this).removeClass('selected');
+                });
+
+                if (typeof caller === "string") {
+                    caller = makeSelector(caller);
+                }
+
+                $(caller).addClass('selected');
+            }
+        }
+        catch (error) {
+
+            console.log(error);
+        }
+    }
+}
+
+function showContent(caller, chapter, section) {
+
+    selectCaller(caller);
+
+    if (chapter) {
+
+        try {
+
+            let target = makeSelector('content');
+
+            $(target).load(chapter);
+
+            $(target).ready(function () {
+
+                // As first bring current target into view.
+                $(target).scrollTop(0);
+
+                if (section) {
+
+                    section = makeSelector(section);
+
+                    let offset = $(section).offset().top - $(target).offset().top;
+                    $(target).scrollTop(offset);
+                }
+            });
+        }
+        catch (error) {
+            debugger;
+            console.log(error);
+        }
+    }
+
+    return false;
+}
